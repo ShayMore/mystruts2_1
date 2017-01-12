500cf1e4077c65fb5a8b65694c8e6aa0e44d5dc8
ShayMore <shaymoo@163.com>
2017-01-12 14:52:29 +0800

    添加Log4j日志打印功能

--- src/com/momo/struts/action/DynamicResultAction.java
+++ src/com/momo/struts/action/DynamicResultAction.java
@@ -1,5 +1,9 @@
 package com.momo.struts.action;
 
+import org.eclipse.jdt.internal.compiler.ast.ThisReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 /**
@@ -13,6 +17,7 @@ public class DynamicResultAction extends ActionSupport {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	private String t;
 	private int flag;
 
@@ -33,13 +38,18 @@ public class DynamicResultAction extends ActionSupport {
 	}
 
 	public String execute() {
+		System.out.println("啦啦~");
+		logger.info("[动态结果集]点击了t="+t);
 		if("1".equals(t)) {
+			logger.info("[动态结果集]进入If1");
 			t = "dynamic1.jsp";
 			flag = 1;
 		} else if("2".equals(t)){
+			logger.info("[动态结果集]进入If2");
 			t = "dynamic2.jsp";
 			flag = 2;
 		}
+		logger.info("[动态结果集]execute结束");
 		return SUCCESS;
 	}
 
